27b2baf264751eae2b956e850f175aa827c32229
Mindaugas132 <mindaugas132@outlook.com>
2024-05-07 11:10:58 +0300

    add lombok annotations to all entities

--- app/src/main/java/lt/mindaugas/androidrestapi/entity/Support.java
+++ app/src/main/java/lt/mindaugas/androidrestapi/entity/Support.java
@@ -2,6 +2,11 @@ package lt.mindaugas.androidrestapi.entity;
 
 import com.google.gson.annotations.SerializedName;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class Support {
     @SerializedName("url")
     private String url;
